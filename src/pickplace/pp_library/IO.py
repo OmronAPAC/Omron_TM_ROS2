10b22146a759685ff269cd3953b87ad864e4abae
guanyewtan <guanyewtan@gmail.com>
2021-07-01 13:43:37 +0800

    Working version for MoMa load/unload

--- src/pickplace/pp_library/IO.py
+++ src/pickplace/pp_library/IO.py
@@ -13,8 +13,7 @@ class IOClass:
         #rclpy.init()
         self.io_node = rclpy.create_node('io_python')
         self.set_io = self.io_node.create_client(SetIO, "set_io")
-        while not self.set_io.wait_for_service(timeout_sec=1.0):
-            self.io_node.get_logger().info('set_io service not available, waiting again...')
+        
         self.request = SetIO.Request()
         self.request.module = 1
         self.request.type = 1
@@ -22,12 +21,16 @@ class IOClass:
         
     def open(self):
         self.request.state = 1.0
+        while not self.set_io.wait_for_service(timeout_sec=1.0):
+            self.io_node.get_logger().info('set_io service not available, waiting again...')
         self.set_io.call_async(self.request)
         time.sleep(1)
         #print("Opening")
 
     def close(self):
         self.request.state = 0.0
+        while not self.set_io.wait_for_service(timeout_sec=1.0):
+            self.io_node.get_logger().info('set_io service not available, waiting again...')
         self.set_io.call_async(self.request)
         time.sleep(1)
         #print("Closing")
